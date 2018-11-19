288f13427b7b918b6a0b0802387c66e02eb865d8
Rajneesh Shukla <rajneesh9651@gmail.com>
2018-11-19 13:08:53 +0530

    Setup theme and hamburgor icon

--- app/src/main/java/com/example/rajneeshshukla/letsconnect/MainActivity.java
+++ app/src/main/java/com/example/rajneeshshukla/letsconnect/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
     private RecyclerView mPostList;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,25 @@ public class MainActivity extends AppCompatActivity {
     private void setLayout() {
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mNavigationView = findViewById(R.id.navigation_view);
+
+        // set toolbar
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+        mToogle = new ActionBarDrawerToggle(MainActivity.this, mDrawerLayout ,R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         View mNavView = mNavigationView.inflateHeaderView(R.layout.navigation_header);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToogle.onOptionsItemSelected(item)){
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /** set Action based on menu item selected */
