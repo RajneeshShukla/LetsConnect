ea269f1e2666ac2bbf73a247074cb6165c01fbaf
Rajneesh Shukla <rajneesh9651@gmail.com>
2018-11-19 12:30:23 +0530

    Added dummy navigation drawer

--- app/src/main/java/com/example/rajneeshshukla/letsconnect/MainActivity.java
+++ app/src/main/java/com/example/rajneeshshukla/letsconnect/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.rajneeshshukla.letsconnect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView mPostList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setLayout();
+        setEventOnDrawerMenu();
+    }
+
+    /* Choose which menu item is checked on drawer*/
+    private void setEventOnDrawerMenu() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                userMenuSelector(menuItem);
+                return false;
+            }
+        });
+    }
+
+    /* Setup variables and header on drawer */
+    private void setLayout() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
+        View mNavView = mNavigationView.inflateHeaderView(R.layout.navigation_header);
+    }
+
+    /** set Action based on menu item selected */
+    private void userMenuSelector(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_new_post:
+                Toast.makeText(this, "New Post", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_home:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_friends:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_find_friends:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_messages:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_setting:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
